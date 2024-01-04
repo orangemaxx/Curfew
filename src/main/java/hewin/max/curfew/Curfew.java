ab7a44102b5a0694bf5fe67092eaab4df923a4b2
OrangeMax <maxhewin@outlook.com>
2024-01-05 11:21:05 +1300

    Created project

---
+++ src/main/java/hewin/max/curfew/Curfew.java
@@ -0,0 +1,17 @@
+package hewin.max.curfew;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Curfew extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
